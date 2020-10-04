308233ecb050384d4fcee9400e52911692907622
takuya911 <takuya.engineer911@gmail.com>
2020-10-04 17:39:44 +0900

    create alb

---
+++ alb.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "alb" {
+  name        = "go-pf-alb"
+  description = "go-pf alb"
+  vpc_id      = "${aws_vpc.main.id}"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "go-pf-alb"
+  }
+}
+
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = "${aws_security_group.alb.id}"
+  type = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name               = "go-pf"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
