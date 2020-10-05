8d595ba1a383076180e19051d0a3e756dca4b8f9
takuya911 <takuya.engineer911@gmail.com>
2020-10-05 09:50:07 +0900

    nginx task

---
+++ task.tf
@@ -0,0 +1,32 @@
+resource "aws_ecs_task_definition" "main" {
+  family = "go-pf"
+
+  # データプレーンの選択
+  requires_compatibilities = ["FARGATE"]
+
+  # ECSタスクが使用可能なリソースの上限
+  # タスク内のコンテナはこの上限内に使用するリソースを収める必要があり、メモリが上限に達した場合OOM Killer にタスクがキルされる
+  cpu    = "256"
+  memory = "512"
+
+  # ECSタスクのネットワークドライバ
+  # Fargateを使用する場合は"awsvpc"決め打ち
+  network_mode = "awsvpc"
+
+  # 起動するコンテナの定義
+  # 「nginxを起動し、80ポートを開放する」設定を記述。
+  container_definitions = <<EOL
+[
+  {
+    "name": "nginx",
+    "image": "nginx:1.14",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOL
+}
